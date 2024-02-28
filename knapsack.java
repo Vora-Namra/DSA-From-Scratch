981f5078568ba3af92035aed5275fbb9bf739f80
Namra Vora <124480820+Vora-Namra@users.noreply.github.com>
2024-02-28 21:16:16 +0530

    Add files via upload

---
+++ knapsack.java
@@ -0,0 +1,30 @@
+
+class knapsack { 
+
+	static int max(int a, int b) { return (a > b) ? a : b; } 
+	static int knapSack(int W, int wt[], int val[], int n) 
+	{ 
+	
+		if (n == 0 || W == 0) 
+			return 0; 
+
+		if (wt[n - 1] > W) 
+			return knapSack(W, wt, val, n - 1); 
+
+		else
+			return max(val[n - 1] 
+						+ knapSack(W - wt[n - 1], wt, 
+									val, n - 1), 
+					knapSack(W, wt, val, n - 1)); 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		int profit[] = new int[] { 60, 100, 120, 50, }; 
+		int weight[] = new int[] { 10, 20, 30,40 }; 
+		int W = 50; 
+		int n = profit.length; 
+		System.out.println(knapSack(W, weight, profit, n)); 
+	} 
+} 
+// Time Complexity: O(n*w)
